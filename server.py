ad2244346ebd29a5b2d4f564755f87ce611abf0d
daniel <danielmao4013@gmail.com>
2019-06-08 15:24:33 -0700

    server updates

--- server.py
+++ server.py
@@ -1,9 +1,27 @@
-import http.server
-import socketserver
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
+from io import BytesIO
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("server at port", PORT)
-    httpd.serve_forever()
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
+
