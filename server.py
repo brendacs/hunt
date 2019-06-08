0fda53f1ad08325bc9a67d99eec54edfed51a1ba
Daniel Mao <daniel.mao@berkeley.edu>
2019-06-08 14:06:21 -0700

    server created

---
+++ server.py
@@ -0,0 +1,11 @@
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("server at port", PORT)
+    httpd.serve_forever()
+
+
