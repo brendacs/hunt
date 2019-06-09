06576aa0aa905fb229d5ec8af9f6eeca48b2d6d1
Jack Lin <jackbrooklin@gmail.com>
2019-06-08 19:52:39 -0700

    user+location

--- model/location_model.py
+++ model/location_model.py
@@ -17,9 +17,9 @@ class Location:
 		max_dist = 50 # km
 		within_max_dist_lst = []
 		user_coords   = (latitude, longitude)
-		custom_coords = db_posts.find({"park": park, "custom": True}) # grabs all user-created landmarks in park
+		custom_coords = db_posts.find({"_id": park, "custom": True}) # grabs all user-created landmarks in park
 
-		for loc in user_coords:
+		for loc in custom_coords:
 			loc_coords = (loc.get("latitude"), loc.get("longitude"))
 			dist = geopy.distance.distance(user_coords, loc_coords).km
 			if dist < max_dist:
@@ -33,6 +33,7 @@ class Location:
 		parks = []
 		dists = []
 		pparks = db_posts.find({"custom": False})
+
 		for ppark in pparks:
 			ppark_coords = (ppark.get("latitude"), ppark.get("longitude"))
 			dist = geopy.distance.distance((latitude, longitude), ppark_coords)
@@ -55,7 +56,7 @@ class Location:
 			for row in reader:
 				if (row[0].equals("Name")):
 					continue
-				post = {"park": row[0],
+				post = {"_id": row[0],
 						"latitude": row[1],
 						"longitude": row[2],
 						"custom": False,
@@ -63,7 +64,7 @@ class Location:
 				db_posts.insert_one(post)
 
 	def add_pin(park, latitude, longitude): # user created pins
-		ppark = db_posts.findOne({"park": park, "custom": False})
+		ppark = db_posts.find({"_id": park, "custom": False}).next()
 		ppark_coords = (ppark.get("latitude"), ppark.get("longitude"))
 		if geopy.distance.distance(ppark_coords, (latitude, longitude)).km < 550:
 			post = {"park": park,
