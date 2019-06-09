e79c5b0f18b16805ca1c38a2eaa8832fca17aa2f
wesley <wesleywang2015@gmail.com>
2019-06-08 18:07:44 -0700

    server route

--- model/location_model.py
+++ model/location_model.py
@@ -1,14 +1,30 @@
 import pymongo
 import geopy.distance
+from model.model import Model
 
 
 client = pymongo.MongoClient(
 	"mongodb+srv://admin:adminadmin@cluster0-dhc2n.mongodb.net/test?retryWrites=true&w=majority")
 db = client.test_database
 
-class Location:
+class LocationModel(Model):
+	function_arguments = {
+		"get_pins": {
+			"latitude": str,
+			"longitude": float
+		},
+		"add_pin": {
+			"parkId": str,
+			"name": str,
+			"latitude": float,
+			"longitude": float
+		}
+	}
 
-	def closest_landmark(latitude, longitude, park):
+# TODO: Add pin_info api
+
+	@staticmethod
+	def get_pins(request):
 		# returns the names and coordinates of landmarks within 25 km of user
 		# return format tuple: (name, (latitude, longitude))
 		name = ""
@@ -16,23 +32,21 @@ class Location:
 
 		cursor = db.posts.find({"park": park}) # grabs all landmarks in park
 
-		coords = (latitude, longitude)
+		user_coords = (latitude, longitude)
 
 		within_max_dist_lst = []
 
 		for loc in cursor:
 			loc_coords = (loc.get("latitude"), loc.get("longitude"))
 
-			dist = geopy.distance.vincenty(user_coords, loc_coords).km
+			dist = geopy.distance.distance(user_coords, loc_coords).km
 
 			if dist < max_dist:
 				within_max_dist_lst.append((loc.get("name"), loc_coords))
 
 		return within_max_dist_lst
 
-	def closest_park(latitude, logitude):
-		# do this wesley
-
+	@staticmethod
 	def add_pin(park, name, latitude, longitude):
 		post = {"park": park,
 				"name": name,
@@ -41,5 +55,3 @@ class Location:
 		}
 		db.posts.insert_one(post)
 		# send to db
-
-
